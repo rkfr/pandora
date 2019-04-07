ebdc37f8853b716d1e8836b4adf4eb0d09b76602
rkfr <e.xbyvnqn@gmail.com>
2019-04-07 22:38:47 +0300

    added modal window control functions

--- src/js/LargeModalWindow/LargeModalWindow.js
+++ src/js/LargeModalWindow/LargeModalWindow.js
@@ -1,10 +1,42 @@
 import Tabs from './Tabs/Tabs';
 
-// const largeModalWindowParams = {
+function startLargeModal(el) {
+    el.addEventListener('click', () => {
+        const {productId: data} = el.dataset,
+            modalWinndowSelector = `[data-modal-window-id="${data}"]`,
+            modalWindow = document.querySelector(modalWinndowSelector);
 
-// }
+        showModalWindow(modalWindow);
+    });
+}
 
+function showModalWindow(el) {
+    const modalWrapper = document.getElementById('modal-windows'),
+        body = document.querySelector('body');
+
+    body.classList.add('overflow-off');    
+    modalWrapper.classList.add('modal-active');
+    el.classList.add('modal-active');
+
+    closeModalWindow({body, el, modalWrapper});
+}
+
+function closeModalWindow({body, el: modalWindow, modalWrapper}) {    
+    const closeButton = modalWindow.querySelector('.close-button');
+
+    closeButton.addEventListener('click', () => {
+        body.classList.remove('overflow-off');
+        modalWrapper.classList.remove('modal-active');
+        modalWindow.classList.remove('modal-active');
+    });
+};
+
+// inicialize tabs for one window
 const containerIs670 = document.getElementById("is-670"),
     is670Tabs = new Tabs(containerIs670);
 
-  
+// find current botton to start window
+const  buttonToStartIs670 = document.querySelector('[data-product-id="is-670"]');
+
+// launching event loop
+startLargeModal(buttonToStartIs670);
